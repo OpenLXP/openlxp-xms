57248512a50ab846e1ef753dea71292c0e4cee6f
JDTobin <90791836+JDTobin@users.noreply.github.com>
2021-12-14 13:28:00 -0500

    Add openlxp-authentication Adds authentication package Updates nginx configuration to use sockets

--- app/openlxp_xms_project/urls.py
+++ app/openlxp_xms_project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -20,5 +21,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),
+    url('', include('openlxp_authentication.urls')),
 ]
