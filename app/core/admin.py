0653b285740c78e0b6f4adc361dce52b5fe44274
rglasgow-deloitte <rglasgow@deloitte.com>
2022-02-18 08:50:10 -0500

    Refactored XMS configurations into configurations app

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,20 +1,15 @@
-from core.models import (ReceiverEmailConfiguration, SenderEmailConfiguration,
-                         XMSConfiguration)
+from core.models import (
+    ReceiverEmailConfiguration,
+    SenderEmailConfiguration,
+)
 from django.contrib import admin
-
-
-# Register your models here.
-@admin.register(XMSConfiguration)
-class XMSConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'target_xis_metadata_api', 'xis_catalogs_api',)
-    fields = [('target_xis_metadata_api', 'xis_catalogs_api',)]
 
 
 @admin.register(ReceiverEmailConfiguration)
 class ReceiverEmailConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('email_address',)
+    list_display = ("email_address",)
 
 
 @admin.register(SenderEmailConfiguration)
 class SenderEmailConfigurationAdmin(admin.ModelAdmin):
-    list_display = ('sender_email_address',)
+    list_display = ("sender_email_address",)
