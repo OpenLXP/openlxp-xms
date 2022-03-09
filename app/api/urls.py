b12607dc7cfb4670fde8a037c8b24faae006ce23
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-09 16:50:31 -0500

    Updates endpoint for urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -7,12 +7,7 @@ app_name = "api"
 
 urlpatterns = [
     path("catalogs/", XISAvailableCatalogs.as_view(), name="catalogs"),
-    path('catalogs/<str:provider_id>/', XISCatalog.as_view(), name='catalog'),
-    path(
-        "catalogs/<str:provider_id>",
-        XISCatalog.as_view(),
-        name="catalog-experiences",
-    ),
+    path('catalogs/<str:provider_id>', XISCatalog.as_view(), name='catalog'),
     path(
         "catalogs/<str:provider_id>/<str:experience_id>/",
         XISExperience.as_view(),
