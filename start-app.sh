a60c5fdac25453084a48ca53011618bf271f098d
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2024-09-10 17:52:37 -0700

    modifying deployment for Ironbank

--- start-app.sh
+++ start-app.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
+cd /tmp/openlxp-xms/app
 python manage.py waitdb 
 python manage.py migrate 
+python manage.py collectstatic --no-input
 python manage.py loaddata admin_theme_data.json 
-cd /tmp/app/ 
+cd /tmp/
 pwd 
 ./start-server.sh
--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd openlxp-xms; python manage.py createsuperuser --no-input)
+    (cd /tmp/openlxp-xms/app; python manage.py createsuperuser --no-input)
 fi
-(cd openlxp-xms; gunicorn openlxp_xms_project.wsgi --reload --user 1001 --bind 0.0.0.0:8000 --workers 3)
+(cd /tmp/openlxp-xms/app; gunicorn openlxp_xms_project.wsgi --reload --user 1001 --bind 0.0.0.0:8000 --workers 3)
