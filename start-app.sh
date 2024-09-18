7582e0302c4e77fac5033b0f70950fdd46661780
kjijo <kjijo@deloitte.com>
2024-09-18 08:45:00 -0500

    add collectstatic

--- start-app.sh
+++ start-app.sh
@@ -2,6 +2,8 @@
 
 python manage.py waitdb 
 python manage.py migrate 
+python manage.py createcachetable 
+python manage.py collectstatic --no-input --clean 
 python manage.py loaddata admin_theme_data.json 
 cd /opt/app/ 
 pwd 
